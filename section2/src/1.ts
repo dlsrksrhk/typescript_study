279d81f5f4062619e07bcad4f4b83807e361df73
sweet.dh <dlsrksrhk@gmail.com>
2025-06-03 11:26:47 +0900

    기본타입 실습
    타입선언 및 배열

---
+++ section2/src/1.ts
@@ -0,0 +1,12 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+let hello: null = null;
+let world: undefined = undefined;
+let numA: number = null;
---
+++ section2/src/2.ts
@@ -0,0 +1,19 @@
+let numArr: number[] = [1, 2, 3, 4, 5];
+let strArr: Array<string> = ["a", "b", "c", "d", "e"];
+
+let multiTypeArr: (number | string)[] = [1, "a", 2, "b", 3];
+let multiTypeArr2: Array<number | string> = [1, "a", 2, "b", 3];
+
+let multiDemensionalArr: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let user: [string, number] = ["Alice", 25];
+
+let users: [string, number][] = [
+  ["Alice", 25],
+  ["Bob", 30],
+  ["Charlie", 35],
+];
